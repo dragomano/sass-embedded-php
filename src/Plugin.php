b76b9fe73e5a30ea38418101057b4024dab0bb5d
Bugo <cheetah@bk.ru>
2025-09-08 10:50:38 +0500

    Convert to Composer plugin

---
+++ src/Plugin.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace Bugo\Sass;
+
+use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\Installer\PackageEvents;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+class Plugin implements PluginInterface, EventSubscriberInterface
+{
+    public function activate(Composer $composer, IOInterface $io): void {}
+
+    public function deactivate(Composer $composer, IOInterface $io): void {}
+
+    public function uninstall(Composer $composer, IOInterface $io): void {}
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            PackageEvents::POST_PACKAGE_INSTALL => 'onPostPackageInstall',
+        ];
+    }
+
+    public function onPostPackageInstall(): void
+    {
+        Installer::postInstall();
+    }
+}
